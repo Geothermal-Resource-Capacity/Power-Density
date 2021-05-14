adef4f58b93f11b8428df76319dc21a4ed405a4c
Jan Niederau <jan.niederau@rwth-aachen.de>
2021-05-14 16:49:13 +0200

    move cumulative_conf function to top of the file

--- streamlit/Power-Density_streamlit.py
+++ streamlit/Power-Density_streamlit.py
@@ -36,6 +36,37 @@ def download_link(object_to_download, download_filename, download_link_text):
     return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
 
+def calculate_cumulative_conf(areaP90: float=1., areaP10: float=10., pdP90: float=10., pdP10: float=24):
+    """Calculate cumulative confidence level for expected development size in MW
+
+    Args:
+        areaP90 (float): pessimistic area in sqkm
+        areaP10 (float): optimistic area in sqkm
+        pdP90 (float): pessimistic power density in MWe/sqkm
+        pdP10 (float): optimistic power density in MWe/sqkm
+
+    Returns:
+        prob_df (pandas Dataframe): cumulative confidence curve in Reservoir Size
+    """
+    # calculate area > 250 °C
+    area_mu = ((np.log(areaP90)+np.log(areaP10))/2)
+    area_sigma = (np.log(areaP10)-np.log(areaP90))/((norm.ppf(0.9)-(norm.ppf(0.1))))
+
+    # calculate powerdensity mean and standard dev
+    powerdens_mu = ((np.log(pdP90)+np.log(pdP10))/2)
+    powerdens_sigma = (np.log(pdP10)-np.log(pdP90))/((norm.ppf(0.9)-(norm.ppf(0.1))))
+
+
+    capacity_mu = area_mu + powerdens_mu
+    capacity_sigma = ((area_sigma**2)+(powerdens_sigma**2))**0.5
+    eds = [lognorm.ppf(x/100, capacity_sigma, loc=0, scale=np.exp(capacity_mu)) for x in range(0,100)]
+    indx = list(np.arange(0,100)[::-1])
+    edsepc_tups = list(zip(indx,eds))
+    prob_df = pd.DataFrame(edsepc_tups, columns = ['Cumulative confidence (%)', 'expected development size (MW)'])
+
+    return prob_df
+
+
 st.title('Power Density')
 st.write('**1: Exploration, is it there?**') #Streamlit uses markdown for formatting
 
@@ -92,36 +123,6 @@ PowerDens_P10 = int(colB.text_input("Power Density P10:", 24))
 
 
 ## CALCULATIONS ################################################
-def calculate_cumulative_conf(areaP90: float=1., areaP10: float=10., pdP90: float=10., pdP10: float=24):
-    """Calculate cumulative confidence level for expected development size in MW
-
-    Args:
-        areaP90 (float): pessimistic area in sqkm
-        areaP10 (float): optimistic area in sqkm
-        pdP90 (float): pessimistic power density in MWe/sqkm
-        pdP10 (float): optimistic power density in MWe/sqkm
-
-    Returns:
-        prob_df (pandas Dataframe): cumulative confidence curve in Reservoir Size
-    """
-    # calculate area > 250 °C
-    area_mu = ((np.log(areaP90)+np.log(areaP10))/2)
-    area_sigma = (np.log(areaP10)-np.log(areaP90))/((norm.ppf(0.9)-(norm.ppf(0.1))))
-
-    # calculate powerdensity mean and standard dev
-    powerdens_mu = ((np.log(pdP90)+np.log(pdP10))/2)
-    powerdens_sigma = (np.log(pdP10)-np.log(pdP90))/((norm.ppf(0.9)-(norm.ppf(0.1))))
-
-
-    capacity_mu = area_mu + powerdens_mu
-    capacity_sigma = ((area_sigma**2)+(powerdens_sigma**2))**0.5
-    eds = [lognorm.ppf(x/100, capacity_sigma, loc=0, scale=np.exp(capacity_mu)) for x in range(0,100)]
-    indx = list(np.arange(0,100)[::-1])
-    edsepc_tups = list(zip(indx,eds))
-    prob_df = pd.DataFrame(edsepc_tups, columns = ['Cumulative confidence (%)', 'expected development size (MW)'])
-
-    return prob_df
-
 # Calculate nu and sigma for area > 250 degC (the mean and variance in log units required for specifying lognormal distributions)
 area_nu = ((np.log(Area_P90)+np.log(Area_P10))/2)
 
@@ -139,40 +140,6 @@ capacity_nu = area_nu + powerdens_nu
 
 
 capacity_sigma = ((area_sigma**2)+(powerdens_sigma**2))**0.5
-
-
-# Calculate cumulative confidence curve for expected power capacity (epc)
-
-#prob = [0.1]
-#expected_power_capacity=[]
-#expected_development_size=[]
-#prob_desc = []
-#
-## Specify probability range
-#for i in range(1,100):
-#    prob.append(i)
-#
-#for j in prob:
-#    # Calculate expected development size distribution
-#    eds = lognorm.ppf(j/100, capacity_sigma, loc=0, scale=np.exp(capacity_nu))
-#    expected_development_size.append(eds)
-#    # Calculate power capacity distribution
-#    epc = eds*POSexpl
-#    expected_power_capacity.append(epc)
-#    # Calculate 100-prob for plotting descending cumulative probability
-#    desc = 100-j
-#    prob_desc.append(desc)
-#    # Print results
-#    #print(j, epc, eds, desc)
-## epc - old function
-##epc = [lognorm.ppf(x/100, capacity_nu, capacity_sigma)*POSexpl for x in range(0,100)]
-#
-## eds and epc - new functions
-#eds = [lognorm.ppf(x/100, capacity_sigma, loc=0, scale=np.exp(capacity_nu)) for x in range(0,100)]
-#epc = [lognorm.ppf(x/100, capacity_sigma, loc=0, scale=np.exp(capacity_nu))*POSexpl for x in range(0,100)]
-#indx = list(np.arange(1,101)[::-1])
-#edsepc_tups = list(zip(indx,eds,epc))
-#prob_df = pd.DataFrame(edsepc_tups, columns = ['Cumulative confidence (%)', 'expected development size (MW)', 'expected power capacity (MWe/km2)'])
 
 prob_df = calculate_cumulative_conf(Area_P90, Area_P10, PowerDens_P90, PowerDens_P10)
 
